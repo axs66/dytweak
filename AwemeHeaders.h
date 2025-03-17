e63f7ec1c5b91a5f4915887b4c0d0d8f0f690051
A先生 <142523398+axs66@users.noreply.github.com>
2025-03-17 14:07:57 +0800

    Update AwemeHeaders.h

--- AwemeHeaders.h
+++ AwemeHeaders.h
@@ -1,4 +1,87 @@
+#import <UIKit/UIKit.h>
+#import <Photos/Photos.h>
+
+#define DYYY 100
+
+typedef NS_ENUM(NSInteger, MediaType) {
+    MediaTypeVideo,
+    MediaTypeImage,
+    MediaTypeAudio,
+    MediaTypeHeic
+};
+
+@interface URLModel : NSObject
+@property (nonatomic, strong) NSArray *originURLList;
+@end
+
+@interface DUXToast : NSObject
++ (void)showText:(NSString *)text;
+@end
+
+@interface AWEVideoModel : NSObject
+@property (nonatomic, strong) URLModel *h264URL;
+@property (nonatomic, strong) URLModel *coverURL;
+@end
+
+@interface AWEMusicModel : NSObject
+@property (nonatomic, strong) URLModel *playURL;
+@end
+
+@interface AWEImageAlbumImageModel : NSObject
+@property (nonatomic, strong) NSArray *urlList;
+@end
+
+@interface AWEAwemeModel : NSObject
+@property (nonatomic, assign,readwrite) CGFloat videoDuration;
+@property (nonatomic, strong) AWEVideoModel *video;
+@property (nonatomic, strong) AWEMusicModel *music;
+@property (nonatomic, strong) NSArray<AWEImageAlbumImageModel *> *albumImages;
+@property (nonatomic, assign) NSInteger currentImageIndex;
+@property (nonatomic, assign) NSInteger awemeType;
+@property (nonatomic, strong) NSString *cityCode;
+@property (nonatomic, strong) NSString *ipAttribution;
+@property (nonatomic, strong) id currentAweme;
+@property (nonatomic, copy) NSString *descriptionString;
+@property (nonatomic, assign) BOOL isLive;
+- (BOOL)isLive;
+@end
+
+@interface AWELongPressPanelBaseViewModel : NSObject
+@property (nonatomic, copy) NSString *describeString;
+@property (nonatomic, assign) NSInteger enterMethod;
+@property (nonatomic, assign) NSInteger actionType;
+@property (nonatomic, assign) BOOL showIfNeed;
+@property (nonatomic, copy) NSString *duxIconName;
+@property (nonatomic, copy) void (^action)(void);
+@property (nonatomic, strong) AWEAwemeModel *awemeModel;
+- (void)setDuxIconName:(NSString *)iconName;
+- (void)setDescribeString:(NSString *)descString;
+- (void)setAction:(void (^)(void))action;
+@end
+
+@interface AWELongPressPanelViewGroupModel : NSObject
+@property (nonatomic, assign) NSInteger groupType;
+@property (nonatomic, strong) NSArray *groupArr;
+@end
+
+@interface AWELongPressPanelManager : NSObject
++ (instancetype)shareInstance;
+- (void)dismissWithAnimation:(BOOL)animated completion:(void (^)(void))completion;
+@end
+
+void downloadMedia(NSURL *url, MediaType mediaType, void (^completion)(void));
+void downloadAllImages(NSMutableArray *imageURLs);
+
 @interface AWENormalModeTabBarGeneralButton : UIButton
+@end
+
+@interface AWEProgressLoadingView : UIView
+- (id)initWithType:(NSInteger)arg1 title:(NSString *)arg2;
+- (id)initWithType:(NSInteger)arg1 title:(NSString *)arg2 progressTextFont:(UIFont *)arg3 progressCircleWidth:(NSNumber *)arg4;
+- (void)dismissWithAnimated:(BOOL)arg1;
+- (void)dismissAnimated:(BOOL)arg1;
+- (void)showOnView:(id)arg1 animated:(BOOL)arg2;
+- (void)showOnView:(id)arg1 animated:(BOOL)arg2 afterDelay:(CGFloat)arg3;
 @end
 
 @interface AWENormalModeTabBarBadgeContainerView : UIView
@@ -21,6 +104,7 @@
 
 @interface AWEPlayInteractionViewController : UIViewController
 @property (nonatomic, strong) UIView *view;
+- (void)performCommentAction;
 @end
 
 @interface UIView (Transparency)
@@ -68,8 +152,15 @@
 
 @end
 
-@interface AWEPlayInteractionProgressController : UIView
+@interface AWEPlayInteractionNewBaseController : UIView
+@property (retain, nonatomic) AWEAwemeModel * model;
+@end
+
+@interface AWEPlayInteractionProgressController : AWEPlayInteractionNewBaseController
 - (UIViewController *)findViewController:(UIViewController *)vc ofClass:(Class)targetClass;
+@property (retain, nonatomic) id progressSlider;
+- (NSString *)formatTimeFromSeconds:(CGFloat)seconds;
+- (NSString *)convertSecondsToTimeString:(NSInteger)totalSeconds;
 @end
 
 @interface AWEAdAvatarView : UIView
@@ -86,13 +177,6 @@
 
 @interface AWEFeedLiveMarkView : UIView
 
-@end
-
-@interface AWEAwemeModel : NSObject
-@property (nonatomic, copy) NSString *ipAttribution;
-@property (nonatomic, copy) NSString *cityCode;
-@property (nonatomic, assign) BOOL isLive;
-@property (nonatomic, strong) AWEAwemeModel *currentAweme;
 @end
 
 @interface AWEPlayInteractionTimestampElement : UIView
@@ -144,4 +228,89 @@
 @end
 
 @interface CommentInputContainerView : UIView
+@end
+
+@interface AWELongPressPanelTableViewController : UIViewController
+@property (nonatomic, strong) AWEAwemeModel *awemeModel;
+@end
+
+@interface AWEModernLongPressPanelTableViewController : UIViewController
+@property (nonatomic, strong) AWEAwemeModel *awemeModel;
+@end
+
+@interface AWESettingSectionModel : NSObject
+@property (nonatomic, copy) NSString *sectionHeaderTitle;
+@property (nonatomic, assign) CGFloat sectionHeaderHeight;
+@property (nonatomic, assign) NSInteger type;
+@property (nonatomic, strong) NSArray *itemArray;
+@end
+
+@interface AWESettingItemModel : NSObject
+@property (nonatomic, copy) NSString *identifier;
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, copy) NSString *detail;
+@property (nonatomic, assign) NSInteger type;
+@property (nonatomic, copy) NSString *iconImageName;
+@property (nonatomic, assign) NSInteger cellType;
+@property (nonatomic, assign) NSInteger colorStyle;
+@property (nonatomic, assign) BOOL isEnable;
+@property (nonatomic, copy) void (^cellTappedBlock)(void);
+@end
+
+@interface AWESettingsViewModel : NSObject
+@property (nonatomic, weak) UIViewController *controllerDelegate;
+@property (nonatomic, copy) NSString *traceEnterFrom;
+@end
+
+@interface DYYYSettingViewController : UIViewController
+@end
+
+@interface AWEElementStackView : UIView
+@property (nonatomic, copy) NSString *accessibilityLabel;
+@property (nonatomic, assign) CGRect frame;
+@property (nonatomic, strong) NSArray *subviews;
+@property (nonatomic, assign) CGAffineTransform transform;
+@end
+
+@interface AWECommentImageModel : NSObject
+@property (nonatomic, copy) NSString *originUrl;
+@end
+
+@class AWECommentModel;
+@class AWECommentLongPressPanelParam;
+@class AWEIMStickerModel;
+@class AWEURLModel;
+
+@interface AWECommentLongPressPanelContext : NSObject
+- (AWECommentModel *)selectdComment;
+- (AWECommentLongPressPanelParam *)params;
+@end
+
+@interface AWECommentLongPressPanelParam : NSObject
+- (AWECommentModel *)selectdComment;
+@end
+
+@interface AWECommentModel : NSObject
+- (AWEIMStickerModel *)sticker;
+@end
+
+@interface AWEIMStickerModel : NSObject
+- (AWEURLModel *)staticURLModel;
+@end
+
+@interface AWEURLModel : NSObject
+- (NSArray *)originURLList;
+@end
+
+@interface _TtC33AWECommentLongPressPanelSwiftImpl37CommentLongPressPanelSaveImageElement : NSObject
+- (AWECommentLongPressPanelContext *)commentPageContext;
+@end
+
+
+
+
+@interface AWEFeedProgressSlider : UIView
+@property (nonatomic, strong) UIView *leftLabelUI;
+@property (nonatomic, strong) UIView *rightLabelUI;
+@property (nonatomic) AWEPlayInteractionProgressController * progressSliderDelegate;
 @end
