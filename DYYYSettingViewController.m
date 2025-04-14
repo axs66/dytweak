c57015ecbd28e29a575307f33a36d9c1b4605e63
A先生 <142523398+axs66@users.noreply.github.com>
2025-04-14 16:40:39 +0800

    Update DYYYSettingViewController.m

--- DYYYSettingViewController.m
+++ DYYYSettingViewController.m
@@ -53,7 +53,7 @@ typedef NS_ENUM(NSInteger, DYYYSettingItemType) {
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.title = @"抖音图层";
+    self.title = @"抖音净化";
     self.expandedSections = [NSMutableSet set];
     self.isAgreementShown = NO;
     
