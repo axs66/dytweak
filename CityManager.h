73fb483a4a98aeb8bbdc45851c724fffa56e71b2
A先生 <142523398+axs66@users.noreply.github.com>
2025-04-18 16:06:30 +0800

    Update CityManager.h

--- CityManager.h
+++ CityManager.h
@@ -5,8 +5,17 @@
 @property (nonatomic, strong) NSDictionary *cityCodeMap;
 
 + (instancetype)sharedInstance;
+
+/// 获取最具体层级的行政区名称
 - (NSString *)getCityNameWithCode:(NSString *)code;
+
+/// 获取省级名称
 - (NSString *)getProvinceNameWithCode:(NSString *)code;
+
+/// 获取完整行政路径
+- (NSString *)getFullCityNameWithCode:(NSString *)code;
+
+/// 加载行政区数据
 - (void)loadCityData;
 
-@end 
+@end
