8266bc002f8142c0df424dd27ffad36090cd1477
A先生 <142523398+axs66@users.noreply.github.com>
2025-05-02 20:06:17 +0800

    Update DYYYConstants.h

--- DYYYConstants.h
+++ DYYYConstants.h
@@ -2,11 +2,11 @@
 #define DYYYConstants_h
 
 // 插件名称常量
-#define DYYY_NAME @"DYYY"
+#define DYYY_NAME @"抖音净化"
 #define DYYY_SETTINGS_NAME @"DYYY设置"
 
 // 版本信息
-#define DYYY_VERSION @"2.2-4"
-#define DYYY_VERSION_STRING @"v2.2-4"
+#define DYYY_VERSION @"3.0.5"
+#define DYYY_VERSION_STRING @"v3.0.5"
 
 #endif
