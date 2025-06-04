10ff603bbc9ab967382bd3407e4068d59addcca3
A先生 <142523398+axs66@users.noreply.github.com>
2025-06-04 18:44:58 +0800

    Update AWESettingItemModel.h

--- AWESettingItemModel.h
+++ AWESettingItemModel.h
@@ -3,8 +3,12 @@
 @interface AWESettingItemModel : NSObject
 
 @property (nonatomic, copy) NSString *identifier;
+@property (nonatomic, copy) NSString *title;
 @property (nonatomic, copy) NSString *detail;
 @property (nonatomic, assign) NSInteger type;
+@property (nonatomic, assign) NSInteger cellType;
+@property (nonatomic, assign) NSInteger colorStyle;
+@property (nonatomic, assign) BOOL isEnable;
 @property (nonatomic, copy) NSString *svgIconImageName;
 
 @end
