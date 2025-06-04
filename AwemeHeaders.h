538250aeb3384624c1c072c1542b8f143a3ceec4
A先生 <142523398+axs66@users.noreply.github.com>
2025-06-04 17:18:32 +0800

    Update AwemeHeaders.h

--- AwemeHeaders.h
+++ AwemeHeaders.h
@@ -55,12 +55,6 @@ static void showIconOptionsDialog(NSString *title, UIImage *previewImage, NSStri
 	[self cleanupTempFile];
 }
 
-// MediaDefines.h
-typedef NS_ENUM(NSUInteger, MediaType) {
-    MediaTypeImage,
-    MediaTypeVideo,
-    MediaTypeLivePhoto
-};
 
 // 添加清理临时文件的方法
 - (void)cleanupTempFile {
